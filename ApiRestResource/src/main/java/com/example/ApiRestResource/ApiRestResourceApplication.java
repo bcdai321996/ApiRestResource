c51ee45f68600e0bc263897b7135ae63e503e822
bui dat <daibc321996@gmail.com>
2022-02-10 00:28:02 +0700

    Initial commit

---
+++ ApiRestResource/src/main/java/com/example/ApiRestResource/ApiRestResourceApplication.java
@@ -0,0 +1,19 @@
+package com.example.ApiRestResource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+public class ApiRestResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestResourceApplication.class, args);
+	}
+
+
+
+}
---
+++ ApiRestResource/src/test/java/com/example/ApiRestResource/ApiRestResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ApiRestResource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestResourceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
